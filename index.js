9891455c1925dbb222285e46c3762f907cef925e
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-03-19 15:59:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Safari';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
